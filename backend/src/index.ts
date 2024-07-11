11b8dc35fddb8eb688dc72f06e93d0757f8dbce1
abelzavaleta <abelzavaleta08@gmail.com>
2024-07-11 03:01:45 -0400

    Backend: db setup config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,31 @@
 import app from './app';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { exit } from 'process';
 
-const { NODE_ENV, HOST, PORT } = process.env;
-app.listen(PORT, () => {
-  console.log(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
-});
+const { NODE_ENV, HOST, PORT, DB_API_KEY } = process.env;
+
+const firebaseConfig = {
+  apiKey: DB_API_KEY,
+  authDomain: 'atom-challenge-d97de.firebaseapp.com',
+  projectId: 'atom-challenge-d97de',
+  storageBucket: 'atom-challenge-d97de.appspot.com',
+  messagingSenderId: '812434207375',
+  appId: '1:812434207375:web:1ed787c0a880776a85f6fa',
+};
+
+try {
+  const firebase = initializeApp(firebaseConfig);
+  const db : any = getFirestore(firebase);
+
+  console.log('Firestore initialized successfully:', firebaseConfig.projectId);
+
+  app.listen(PORT, () => {
+    console.log(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
+  });
+
+} catch (error) {
+  console.error('Error initializing the server:', error);
+  exit()
+}
+
